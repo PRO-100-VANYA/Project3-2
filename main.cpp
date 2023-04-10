786315648da5b100d97848037459064bfa745ebe
PRO-100-VANYA <rindin-vanya2008@yandex.ru>
2023-04-10 19:25:29 +0300

    dobavil otdelnye faily

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include "inout.h"
+#include <algorithm>
+int main() 
+{
+    setlocale(0, "");
+    vector<int> vec;
+    read(vec);
+    sort(vec.begin(), vec.end());
+    print(vec);
+    return 0;
+}
